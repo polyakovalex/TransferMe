a74e18d429fbeb7e4b880133fd9f035a93f95ec2
APolyakov <apolyakov@unitarius.ru>
2019-10-01 10:44:01 +0300

    Add step autocoplit

--- app/src/main/java/ru/polyakov/transferme/MainActivity.java
+++ app/src/main/java/ru/polyakov/transferme/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.polyakov.transferme;
 
 import android.os.Bundle;
+import android.view.View;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
--- app/src/main/java/ru/polyakov/transferme/ui/home/HomeFragment.java
+++ app/src/main/java/ru/polyakov/transferme/ui/home/HomeFragment.java
@@ -4,30 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import ru.polyakov.transferme.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment{
 
     private HomeViewModel homeViewModel;
+    private EditText etFrom,etTo;
+    private Button btSearch;
+    NavController navController;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+       // navController = Navigation.findNavController(root);
+
+        etFrom = root.findViewById(R.id.et_from);
+        etFrom.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.autoComplitFragment));
+
+       /* final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
+
+   /* @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.et_from:
+               // Navigation.findNavController(v).navigate(R.id.autoComplitFragment);
+                break;
+        }
+
+
+    }*/
 }
